8bb2dc1447e4d536ee85914ce7389f573a4b6c83
jsphrestrepo <71779660+jsphrestrepo@users.noreply.github.com>
2020-10-21 17:23:21 -0400

    adding java script

---
+++ styles/main.js
@@ -0,0 +1,13 @@
+function greet(){
+    alert("Hi! Welcome to my website");
+}
+  
+function getName(){
+    let name = prompt("What's your name?");
+    alert("Nice to meet you, " + name);
+}
+
+function askHowTheyAre() {
+    let good = prompt("How are you?")
+    alert("That's very good to hear, " + good)
+}
