670fb283c1e6a0df47e9836520dcde0fe69f7243
jsphrestrepo <71779660+jsphrestrepo@users.noreply.github.com>
2020-10-28 20:07:06 -0400

    buttons

--- styles/main.js
+++ styles/main.js
@@ -1,13 +1,40 @@
-function greet(){
-    alert("Hi! Welcome to my website");
+function function1(){
+    alert("function 1 coming soon!");
 }
   
-function getName(){
-    let name = prompt("What's your name?");
-    alert("Nice to meet you, " + name);
+function function2(){
+    alert("function 2 coming soon!");
 }
 
-function askHowTheyAre() {
-    let good = prompt("How are you?")
-    alert("That's very good to hear, " + good)
+function function3() {
+    alert("function 3 coming soon!");
 }
+
+function function4() {
+    alert("function 4 coming soon!");
+}
+
+function function5() {
+    alert("function 5 coming soon!");
+}
+
+function function6() {
+    alert("function 6 coming soon!");
+}
+
+function function7() {
+    alert("function 7 coming soon!");
+}
+
+function function8() {
+    alert("function 8 coming soon!");
+}
+
+function function9() {
+    alert("function 9 coming soon!");
+}
+
+function function10() {
+    alert("function 10 coming soon!");
+}
+    
