2e6e0e4375a5aaa7ba906652cc5a6fc13cafd775
jsphrestrepo <71779660+jsphrestrepo@users.noreply.github.com>
2020-10-29 13:27:50 -0400

    Adding comment to button

--- styles/main.js
+++ styles/main.js
@@ -1,40 +1,50 @@
+//JS for the funtion 1 button//
 function function1(){
-    alert("function 1 coming soon!");
+    alert("Function 1 coming soon!");
 }
-  
+
+//JS for the funtion 2 button//
 function function2(){
-    alert("function 2 coming soon!");
+    alert("Function 2 coming soon!");
 }
 
+//JS for the funtion 3 button//
 function function3() {
-    alert("function 3 coming soon!");
+    alert("Function 3 coming soon!");
 }
 
+//JS for the funtion 4 button//
 function function4() {
-    alert("function 4 coming soon!");
+    alert("Function 4 coming soon!");
 }
 
+//JS for the funtion 5 button//
 function function5() {
-    alert("function 5 coming soon!");
+    alert("Function 5 coming soon!");
 }
 
+//JS for the funtion 6 button//
 function function6() {
-    alert("function 6 coming soon!");
+    alert("Function 6 coming soon!");
 }
 
+//JS for the funtion 7 button//
 function function7() {
-    alert("function 7 coming soon!");
+    alert("Function 7 coming soon!");
 }
 
+//JS for the funtion 8 button//
 function function8() {
-    alert("function 8 coming soon!");
+    alert("Function 8 coming soon!");
 }
 
+//JS for the funtion 9 button//
 function function9() {
-    alert("function 9 coming soon!");
+    alert("Function 9 coming soon!");
 }
 
+//JS for the funtion 10 button//
 function function10() {
-    alert("function 10 coming soon!");
+    alert("Function 10 coming soon!");
 }
     
